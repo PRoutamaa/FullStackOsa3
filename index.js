3dd5ae81322e0b9190ee82651d6ea27f0b39dd87
PRoutamaa <89683328+PRoutamaa@users.noreply.github.com>
2024-11-20 13:38:04 +0200

    fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
